53963408e9bae28668135bebe2f1ca448f944132
Shankara <=>
2020-05-08 21:12:06 +0530

    Modified filter fucntion.
    Used memcpy instead realloc

--- array.c
+++ array.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include "array.h"
 
 Array *create_array(int length) {
@@ -8,10 +9,7 @@ Array *create_array(int length) {
     return NULL;
   }
   new_array->length = length;
-  new_array->array = malloc(length * sizeof(int));
-  if(new_array->array == NULL) {
-    return NULL;
-  }
+  new_array->array = length ? malloc(length * sizeof(int)) : NULL;
   return new_array;
 }
 
@@ -27,20 +25,22 @@ Array *map(Array *array, Mapper mapper) {
 }
 
 Array *filter(Array *array, Predicate predicate) {
-  Array *filtered_array = create_array(array->length);
-  if(filtered_array == NULL) {
-    return NULL;
-  }
+  int temp[array->length];
   int filtered_array_index = 0;
   for(size_t i = 0; i < array->length; i++) {
     Bool result = (*predicate)(array->array[i]);
     if(result) {
-      filtered_array->array[filtered_array_index] = array->array[i];
+      temp[filtered_array_index] = array->array[i];
       filtered_array_index++;
     }
   }
-  filtered_array->array = realloc(filtered_array->array, sizeof(int) * filtered_array_index);
-  filtered_array->length = filtered_array_index;
+  Array *filtered_array = create_array(filtered_array_index);
+  if(filtered_array == NULL) {
+    return NULL;
+  }
+  if(filtered_array->length != 0) {
+    memcpy(filtered_array->array, temp, filtered_array_index * sizeof(int));
+  }
   return filtered_array;
 }
 
