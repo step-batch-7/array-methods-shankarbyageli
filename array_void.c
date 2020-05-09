5748963ee79bff737538de363dd9725f60cf83a2
Shankara <=>
2020-05-09 18:43:30 +0530

    Added filter_void and reduce_void to array_void.c

--- array_void.c
+++ array_void.c
@@ -23,3 +23,31 @@ ArrayVoid_ptr map_void(ArrayVoid_ptr src, MapperVoid mapper) {
   }
   return mapped;
 }
+
+ArrayVoid_ptr filter_void(ArrayVoid_ptr src, PredicateVoid predicate) {
+  ArrayVoid_ptr filtered = create_void_array(src->length);
+  if(filtered == NULL) {
+    return NULL;
+  }
+  int filter_index = 0;
+  for(size_t i = 0; i < src->length; i++) {
+    if((*predicate)(src->array[i])) {
+      filtered->array[filter_index] = src->array[i];
+      filter_index++;
+    }
+  }
+  if(filter_index != 0) {
+    filtered->array = realloc(filtered->array, sizeof(Object) * filter_index);
+  } else {
+    filtered->array = NULL;
+  }
+  filtered->length = filter_index;
+  return filtered;
+}
+
+Object reduce_void(ArrayVoid_ptr src, Object init, ReducerVoid reducer) {
+  for(size_t i = 0; i < src->length; i++) {
+    init = (*reducer)(init, src->array[i]);
+  }
+  return init;
+}
